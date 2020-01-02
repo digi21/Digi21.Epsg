07db2b6c645290ef6fe21ced59bd13df8b4f0eba
José Ángel Martínez Torres <joseangelmt@outlook.com>
2020-01-02 16:24:41 +0100

    Solved AssemblyTitle attribute

--- Digi21.Epsg/Properties/AssemblyInfo.cs
+++ Digi21.Epsg/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: ReferenceAssembly]
-[assembly: AssemblyTitle("Digi21.OpenGis.Epsg")]
+[assembly: AssemblyTitle("Digi21.Epsg")]
 [assembly: AssemblyDescription("Adds user interface to Digi21.OpenGis")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Digi21.net")]
