5c9d1b088b6036a95e8d5d9eae59c45445741af6
José Ángel Martínez Torres <joseangelmt@outlook.com>
2020-01-02 16:19:05 +0100

    Added public surface of Digi21.Epsg

---
+++ Digi21.Epsg/CreateVerticalTransformationHelper.cs
@@ -0,0 +1,12 @@
+﻿using Digi21.OpenGis.Transformations;
+using System;
+
+namespace Digi21.OpenGis.Epsg
+{
+    public class CreateVerticalTransformationHelper
+    {
+        public CreateVerticalTransformationHelper(IntPtr hwndParent) => throw null;
+        public static IMathTransform DialogCreateVerticalTransformation(string sourceCrs, string destinationCrs) => throw null;
+        public IMathTransform DialogCreateVerticalTransformationWnd(string sourceCrs, string destinationCrs) => throw null;
+    }
+}
---
+++ Digi21.Epsg/SelectTransformationHelper.cs
@@ -0,0 +1,14 @@
+﻿using Digi21.OpenGis.Transformations;
+using System;
+using System.Collections.Generic;
+
+namespace Digi21.OpenGis.Epsg
+{
+    public class SelectTransformationHelper
+    {
+        // Methods
+        public SelectTransformationHelper(IntPtr hwndParent) => throw null;
+        public static string DialogSelectTransformation(string sourceCrs, string destinationCrs, IEnumerable<CoordinateOperation> locatedOperations) => throw null;
+        public string DialogSelectTransformationWnd(string sourceCrs, string destinationCrs, IEnumerable<CoordinateOperation> locatedOperations) => throw null;
+    }
+}
