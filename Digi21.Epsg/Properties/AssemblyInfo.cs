eb3617ae623047b06479b183cffb9b2037652c48
José Ángel Martínez Torres <joseangelmt@outlook.com>
2020-01-02 16:20:00 +0100

    Updated AssemblyInfo with information about the assembly

--- Digi21.Epsg/Properties/AssemblyInfo.cs
+++ Digi21.Epsg/Properties/AssemblyInfo.cs
@@ -5,13 +5,13 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Digi21.Epsg")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("Digi21.OpenGis.Epsg")]
+[assembly: AssemblyDescription("Adds user interface to Digi21.OpenGis")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Digi21.Epsg")]
-[assembly: AssemblyCopyright("Copyright ©  2020")]
-[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCompany("Digi21.net")]
+[assembly: AssemblyProduct("Digi3D.NET")]
+[assembly: AssemblyCopyright("Copyright (c) 2010-2020 Digi21.net")]
+[assembly: AssemblyTrademark("Copyright (c) 2010-2020 José Ángel Martínez Torres")]
 [assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("a3c2ffdb-f353-4f83-ad21-8f612c361846")]
+[assembly: Guid("aaeddb65-2c2c-41b5-b2bf-66b072e3d6a5")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("17.0.0.0")]
+[assembly: AssemblyFileVersion("17.0.0.0")]
